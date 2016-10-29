dc80e41eab0e2c7cb4295d0e473dac5e0d458c1e
Gabriel De La O <gabriel@cohesionlabs.io>
2016-10-29 17:52:43 -0500

    Some more file.

---
+++ mobile-config.js
@@ -0,0 +1,48 @@
+App.info({
+  //id: 'com.angularpulse.gabe.ap',
+  name: 'Fixxly',
+  description: 'A Skull puck app for the Home Insurance Industry.'//,
+  author: 'Doug Boude & Gabriel De La O',
+  //email: '',
+  //website: 'http://fixxly.com'
+});
+
+App.icons({
+  // iOS
+  //'iphone': 'resources/icons/Icon-60@2x.png',
+  //'iphone_2x': 'resources/icons/Icon-60@2x.png',
+  //'iphone_3x': 'resources/icons/Icon-60@3x.png',
+  //'ipad': 'resources/icons/Icon-76.png',
+  //'ipad_2x': 'resources/icons/Icon-76@2x.png'//,
+
+  // Android
+  //'android_ldpi': 'resources/icons/icon-36x36.png',
+  //'android_mdpi': 'resources/icons/icon-48x48.png',
+  //'android_hdpi': 'resources/icons/icon-72x72.png',
+  //'android_xhdpi': 'resources/icons/icon-96x96.png'
+});
+
+App.launchScreens({
+  // iOS
+  //'iphone': 'resources/splash/Default.png',
+  //'iphone_2x': 'resources/splash/Default-667h@2x.png',
+  //'iphone5': 'resources/splash/Default-736h@3x.png',
+  //'iphone6': 'resources/splash/Default-736h@3x.png',
+  //'iphone6p_portrait': 'resources/splash/Default-736h@3x.png',
+  //'iphone6p_landscape': 'resources/splash/Default-Landscape-736h@3x.png'//,
+
+  //'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  //'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  //'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  //'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png'//,
+
+  // Android
+  //'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  //'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  //'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  //'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  //'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  //'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  //'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+});
